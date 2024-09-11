ecc4587cff37a519d93aea5369e2b54e604f97d4
Aparna1407 <aparnadesai7@gmail.com>
2024-09-12 02:03:28 +0530

    c5

---
+++ checking.py
@@ -0,0 +1,13 @@
+n1=int(input("Enter a number: "))
+
+if n1>100:
+
+   print(f"The number {n1} is greater than 100")
+
+elif n1==100:
+
+   print(f"The number {n1} is equal to 100")
+
+else:
+
+   print(f"The number {n1} is less than 100")
